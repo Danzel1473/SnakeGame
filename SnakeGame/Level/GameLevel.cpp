0099e480f26f71a7249834f0aa9fdd9a86559343
Yellllllll <65860528+Danzel1473@users.noreply.github.com>
2025-01-21 10:10:01 +0900

    Draw 문제 확인

--- SnakeGame/Level/GameLevel.cpp
+++ SnakeGame/Level/GameLevel.cpp
@@ -1,34 +1,30 @@
 #include "GameLevel.h"
-#include "string"
+#include <string>
 
 GameLevel::GameLevel()
 {
-    const char* mapString = "1111111111111\n1           1\n1           1\n1           1\n1           1\n1111111111111";
+    std::string mapString = "11\n1 1 1";
+    int x = 0;
+    int y = 0;
 
-    for (int i = 0; i < strlen(mapString); i++)
+    //for (int i = 0; i < strlen(mapString); i++)
+    for (int i = 0; i < mapString.size(); i++)
     {
-        static int x = 0;
-        static int y = 0;
-
         if (mapString[i] == '\n')
         {
-            y++;
+            ++y;
             x = 0;
             continue;
         }
 
         if (mapString[i] == '1')
         {
-            Wall* wall = new Wall(Vector2(y, x));
+            Wall* wall = new Wall(Vector2(x, y));
             walls.push_back(wall);
-            x++;
-            
-            continue;
         }
 
-        x++;
+        ++x;
     }
-
 }
 
 void GameLevel::Update(float deltaTime)
